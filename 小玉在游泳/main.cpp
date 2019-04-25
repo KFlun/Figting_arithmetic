58f67edff4f6a539bd2d2ac83bf04ba217af157a
KnifeofLondon <1628406560@qq.com>
2019-04-25 20:43:02 +0800

    new

---
+++ 小玉在游泳/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int  steps; double tdistans, xdistans = 0; double  xdistans_1 = 2;
+	cin >> tdistans;
+	for (steps = 1; xdistans <= tdistans; steps++)
+	{
+		xdistans += xdistans_1;
+		xdistans_1 = (xdistans_1 / 100) * 98;
+	}
+	cout << steps - 1 << endl;
+	system("pause");
+	return 0;
+}
